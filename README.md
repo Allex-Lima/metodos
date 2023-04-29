bfd670459fe54acb334b705503c35add319b4af9
Alexandro <alexlmangabeira@gmail.com>
2023-04-28 21:52:08 -0300

    Métodos at()

---
+++ README.md
@@ -0,0 +1,5 @@
+#**Método Array.at()**
+>>O método at() recebe um parametro (número) positivo ou negativo referenciando o índice.\
+>>Caso o índice exista no array retorna o item da posicão.\
+>>Caso o índice não exista no array retorna undefined.
+>>>>Referência MDN: [at()](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/at)
---
+++ at.js
@@ -0,0 +1,21 @@
+const nomes = ['Alex', 'Bárbara', 'Carla', 'Daniel', 'Eva']
+
+// Retorna o item do índice/posição passado por parametro
+const paramPositivo = nomes.at(3)
+console.log(paramPositivo)
+
+// Retorno o último item do array, valor negativo
+const paramNegativo = nomes.at(-1)
+console.log(paramNegativo)
+
+// Item não encontrado, índice/posição não existe
+const naoEncontrado = nomes.at(7)
+console.log(naoEncontrado)
+
+// Usando função passando parametro para retorna o valor.
+function nome(n) {
+    return n.at(3)
+}
+
+const retornoFuncao = nome(nomes)
+console.log(retornoFuncao)
